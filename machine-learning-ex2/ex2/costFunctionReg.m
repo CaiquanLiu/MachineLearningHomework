c43ef0d6fdc4afc8c9a00190c5d5558ba3126f46
liucaiquan <liucaiquan@baidu.com>
2018-02-26 16:10:00 +0800

    ex2 finished

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,8 +19,11 @@ grad = zeros(size(theta));
 
 
 
+J=1/m*(-1*y'*log(sigmoid(X*theta))-(1-y)'*log(1-sigmoid(X*theta)))+lambda/(2*m)*sum(theta(2:end).^2);
 
+grad(1,:)=1/m*sum(sigmoid(X*theta)-y);
 
+grad(2:size(theta),:)=1/m*X(:,2:end)'*(sigmoid(X*theta)-y)+lambda/m*theta(2:end,:);
 
 % =============================================================
 
